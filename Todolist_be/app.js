08d4e1ec07c7d482fb33fd7c5b9727fbf66cd8af
OwenSyKim <owen.sykim@gmail.com>
2023-10-31 16:50:19 -0700

    Add toJson

--- Todolist_be/app.js
+++ Todolist_be/app.js
@@ -9,15 +9,14 @@ const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 //Connect Router
 
-require("dotenv").config();//
+require("dotenv").config(); //
 //dotenv
 
 //setup express
 const app = express();
 
-
 //
-const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;//
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD; //
 //
 
 app.use(bodyParser.json());
@@ -26,19 +25,19 @@ app.use(bodyParser.json());
 app.use(cors());
 // use cors
 
-app.use("/api", indexRouter)
+app.use("/api", indexRouter);
 //App using indexRouter, Unneccessary, but for make sure it's api
 
 // const mongoURI = `mongodb://localhost:27017/todoDemo`
-const mongoURI = MONGODB_URI_PROD;//
+const mongoURI = MONGODB_URI_PROD; //
 //
 
 mongoose
-    .connect(mongoURI, {useNewUrlParser: true })
-    .then(() =>  console.log("mongoose connected"))
-    .catch((error) =>  console.log("DB connection fail", error));
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("mongoose connected"))
+  .catch((error) => console.log("DB connection fail", error));
 //connect with mongoose, "useNewUrlParser => Make sure to accept new form of Mongodb"
 
 app.listen(process.env.PORT || 5010, () => {
-    console.log("server on 5010");
+  console.log("server on 5010");
 });
