ecf57980ede7d7ec15ba1b145af1b3ff136e828f
OwenSyKim <owen.sykim@gmail.com>
2023-10-30 23:03:12 -0700

    add Login and JWT token

--- Todolist_be/models/user.js
+++ Todolist_be/models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+
+require('dotenv').config();
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const userSchema = Schema({
     name: {
@@ -14,8 +19,14 @@ const userSchema = Schema({
         type: String,
         required: true
     }
-}, {timestamps: true});
+}, {timestamps: true}
+);
 
-const User = mongoose.model("User", userSchema)
+userSchema.methods.generateToken = function () {
+    var token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY);
+    return token;
+}; // Generate Token
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- Todolist_be/routes/user.api.js
+++ Todolist_be/routes/user.api.js
@@ -6,4 +6,7 @@ const userController = require("../controller/user.controller");
 // 1. Sign-in endpoint, For signin is "Post"
 router.post("/", userController.createUser);
 
+// 2. login endpoint
+router.post("/login", userController.loginWithEmail);
+
 module.exports = router;
