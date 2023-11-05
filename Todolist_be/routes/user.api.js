9210be9847d6f777e5f866f29004fb7334a8fe82
OwenSyKim <owen.sykim@gmail.com>
2023-11-04 22:38:45 -0700

    Add Authenticate BE, Private Routes FE

--- Todolist_be/routes/user.api.js
+++ Todolist_be/routes/user.api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const authController = require("../controller/auth.controller");
 const router = express.Router();
 const userController = require("../controller/user.controller");
 
@@ -7,5 +8,8 @@ router.post("/", userController.createUser);
 
 // 2. login endpoint
 router.post("/login", userController.loginWithEmail);
+
+router.get("/me", authController.authenticate, userController.getUser);
+//authenticate email
 
 module.exports = router;
